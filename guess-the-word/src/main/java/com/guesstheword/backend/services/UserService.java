f3e9ffee1d9f8e094fc9ef7473e87d65488c4bce
ghamani <138302691+Ghamani121@users.noreply.github.com>
2025-09-27 18:48:33 +0530

    Update UserService.java

--- guess-the-word/src/main/java/com/guesstheword/backend/services/UserService.java
+++ guess-the-word/src/main/java/com/guesstheword/backend/services/UserService.java
@@ -46,7 +46,7 @@ public class UserService {
             
             return "Login successful!";
         } else {
-            return user.getHashPassword();
+            return password;
         }
     }
 
